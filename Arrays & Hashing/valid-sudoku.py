eed2d90cbc09fca3f7caf272f685680ada7e905f
Rashil <rashildudhara@gmail.com>
2023-11-13 19:09:09 +0530

    use sets to save redundant memory https://leetcode.com/problems/valid-sudoku/submissions/1097913064

--- Arrays & Hashing/valid-sudoku.py
+++ Arrays & Hashing/valid-sudoku.py
@@ -1,4 +1,4 @@
-# https://leetcode.com/problems/valid-sudoku/submissions/1097901306/
+# https://leetcode.com/problems/valid-sudoku/submissions/1097913064
 # row can have digits betwen 1-9 and . (empty)
 # digit should not be in same row, col or 3x3 grids of the og table 
 # _|_|_||_|_|_||_|_|_
@@ -17,36 +17,16 @@ class Solution:
     BLANK = '.'
 
     def isValidSudoku(self, board: list[list[str]]) -> bool:
-        table_buffer = dict()
+        table_buffer:dict[str, dict[str, set]] = dict()
 
         def calculate_quadrant(row, col) -> int:
-            if (0 <= row <= 2) and (0 <= col <= 2):
-                return 1
-            elif (0 <= row <= 2) and (3 <= col <= 5):
-                return 2
-            elif (0 <= row <= 2) and (6 <= col <= 8):
-                return 3
-
-            elif (3 <= row <= 5) and (0 <= col <= 2):
-                return 4
-            elif (3 <= row <= 5) and (3 <= col <= 5):
-                return 5
-            elif (3 <= row <= 5) and (6 <= col <= 8):
-                return 6
-
-            elif (6 <= row <= 8) and (0 <= col <= 2):
-                return 7
-            elif (6 <= row <= 8) and (3 <= col <= 5):
-                return 8
-            elif (6 <= row <= 8) and (6 <= col <= 8):
-                return 9
-            else:
-                raise ValueError("Invalid grid coordinates!")
+            return (row // 3) * 3 + col // 3 + 1
 
         def update_table_buffer(item: str, row: int, col: int) -> None:
-            table_buffer[item]['ROW'].update({row: True})
-            table_buffer[item]['COL'].update({col: True})
-            table_buffer[item]['QUAD'].update({calculate_quadrant(row, col): True})
+            table_buffer[item]['ROW'].add(row)
+            table_buffer[item]['COL'].add(col)
+            table_buffer[item]['QUAD'].add(calculate_quadrant(row, col))
+
 
         for row, row_item in enumerate(board):
             for pos, item in enumerate(row_item):
@@ -55,21 +35,21 @@ class Solution:
                 elif item not in table_buffer:
                     logging.debug(f"Newly discovered digit -> {item}")
                     table_buffer.update({item: {
-                        'ROW': {},
-                        'COL': {},
-                        'QUAD': {},
+                        'ROW': set(),
+                        'COL': set(),
+                        'QUAD': set(),
                     }})
                     update_table_buffer(item, row, pos)
                 else:
                     logging.debug(f"Check if item is in same 3x3 matrix")
                     logging.debug(f"Check if item is in same row col")
 
-                    if table_buffer[item]['QUAD'] == calculate_quadrant(row, pos):
+                    if calculate_quadrant(row, pos) in table_buffer[item]['QUAD']:
                         return False
 
-                    elif table_buffer[item]['ROW'] == row:
+                    elif row in table_buffer[item]['ROW']:
                         return False
-                    elif table_buffer[item]['COL'] == pos:
+                    elif pos in table_buffer[item]['COL']:
                         return False
                     else:
                         update_table_buffer(item, row, pos)
@@ -79,15 +59,14 @@ class Solution:
 
 # Example usage:
 dummy_input = [
-    [".", ".", "5", ".", ".", ".", ".", ".", "."],
-    [".", ".", ".", "8", ".", ".", ".", "3", "."],
-    [".", "5", ".", ".", "2", ".", ".", ".", "."],
-    [".", ".", ".", ".", ".", ".", ".", ".", "."],
-    [".", ".", ".", ".", ".", ".", ".", ".", "9"],
-    [".", ".", ".", ".", ".", ".", "4", ".", "."],
-    [".", ".", ".", ".", ".", ".", ".", ".", "7"],
-    [".", "1", ".", ".", ".", ".", ".", ".", "."],
-    ["2", "4", ".", ".", ".", ".", "9", ".", "."]
-]
+    ["8","3",".",".","7",".",".",".","."],
+    ["6",".",".","1","9","5",".",".","."],
+    [".","9","8",".",".",".",".","6","."],
+    ["8",".",".",".","6",".",".",".","3"],
+    ["4",".",".","8",".","3",".",".","1"],
+    ["7",".",".",".","2",".",".",".","6"],
+    [".","6",".",".",".",".","2","8","."],
+    [".",".",".","4","1","9",".",".","5"],
+    [".",".",".",".","8",".",".","7","9"]]
 
 print(Solution().isValidSudoku(dummy_input))
